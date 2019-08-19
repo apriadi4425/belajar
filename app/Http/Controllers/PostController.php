4a75bf35de51470da5a303165e55d5fa824d059d
apriadi4425 <apriadi1994@gmail.com>
2019-08-20 05:33:13 +0800

    membuat Route api -get dengan PostController@index 2

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostController extends Controller
 {
-    //
+    public function index(){
+        return Post::paginate(10);
+    }
 }
