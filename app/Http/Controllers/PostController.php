8c29297463afc9bb9b00a087c6931792a1fd5007
apriadi-kantor <apriadi441994@gmail.com>
2019-08-20 16:17:14 +0700

    membuat method insert pada table posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Post;
+use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
 {
@@ -12,5 +13,18 @@ class PostController extends Controller
     }
     public function get_data_by_id(Post $post){
         return $post;
+    }
+    public function insert(Request $request){
+
+        $validator = Validator::make($request->all(), [
+           'title' => 'required|min:5',
+            'content' => 'required|min:10|max:100'
+        ]);
+        if ($validator->fails()) {
+            return response()->json(['status'=> 'error','data'=>$validator->errors()],400);
+        }else{
+            Post::create($request->all());
+        }
+
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,5 @@ use Illuminate\Http\Request;
 
 Route::get('posts','PostController@index');
 Route::get('posts/{post}','PostController@get_data_by_id');
+
+Route::post('posts','PostController@insert');
